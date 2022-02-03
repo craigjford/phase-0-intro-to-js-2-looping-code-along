4f6b14fc1654bd37493b2ae462ce92909c4ed00a
Craig Ford <craig_ford@icloud.com>
2022-02-03 15:34:51 -0500

    commit tree 210a38e550a6222b01b690ab1d19894747f428ce

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-
+/*
 for (let age = 30; age < 40; age++) {
     console.log(`I'm ${age} years old.  Happy birthday to me!`);
 }
@@ -51,8 +51,65 @@ function countDown(number) {
     }
 }
 
-countDown(10);
+let passNum = 10;
+countDown(passNum);
 
+*/
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gift) {
+    for (let i = 0; i < gift.length; i++) {
+        console.log(`Wrapped ${gift[i]} and added a bow!`);      
+    }
+    return gift;
+}  
+
+wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+let messageArray = [];
+let answerArray = [];
+
+function writeCards(array, occasion) {
+    for (let i = 0; i < names.length; i++) {
+         messageArray[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return messageArray;
+}
+
+answerArray = writeCards(names, "birthday");
+
+for (i = 0; i < answerArray.length; i++) {
+    console.log(`${answerArray[i]}`);
+}
+
+function wrapMoreGifts(gifts) {
+    let i  = 0;
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++;
+    }
+}
+
+wrapMoreGifts(gifts);
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i = i - 1;
+    }
+}
+
+let passNum = 10;
+countDown(passNum);
+passNum = passNum;
+countDown(passNum);
 
  
 
